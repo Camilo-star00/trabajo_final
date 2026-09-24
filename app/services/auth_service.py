7cbee2a9320c7dd57b2cf64bdfc0ec2620994657
Camilo-star00 <cg5598547@gmail.com>
2026-09-24 11:56:00 -0500

    Agregado AuthService que faltaba

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,31 +1,18 @@
-from fastapi import HTTPException
-from app.models.servicio_model import ServicioModel
+import jwt
+from datetime import datetime, timedelta
 
-class ServicioService:
-    def __init__(self):
-        self.model = ServicioModel()
-
-    def obtener_todos(self):
-        return self.model.get_all()
-
-    def obtener_por_id(self, servicio_id: int):
-        servicio = self.model.get_by_id(servicio_id)
-        # Si no existe en la BD, lanzamos un 404 en lugar de devolver None
-        if not servicio:
-            raise HTTPException(status_code=404, detail="Servicio no encontrado")
-        return servicio
-
-    def crear(self, nombre: str, precio: float):
-        return self.model.create(nombre, precio)
-
-    def actualizar(self, servicio_id: int, nombre: str, precio: float):
-        actualizado = self.model.update(servicio_id, nombre, precio)
-        if not actualizado:
-            raise HTTPException(status_code=404, detail="Servicio no encontrado")
-        return actualizado
-
-    def eliminar(self, servicio_id: int):
-        eliminado = self.model.delete(servicio_id)
-        if not eliminado:
-            raise HTTPException(status_code=404, detail="Servicio no encontrado")
-        return {"message": "Servicio eliminado correctamente"}
+class AuthService:
+    def login(self, usuario: str, password: str):
+        payload = {
+            "sub": "1",
+            "usuario": usuario,
+            "exp": datetime.utcnow() + timedelta(days=365)
+        }
+        
+        token = jwt.encode(payload, "clave_cualquiera_1234567890_super_larga", algorithm="HS256")
+        
+        return {
+            "mensaje": f"Bienvenido {usuario}, inicio de sesión exitoso",
+            "token": token,
+            "token_type": "bearer"
+        }
